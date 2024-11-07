b75e013a95a59dfed40a300eccdd04d64c3995cd
Maria Gutierrez <majo20gutierrez@gmail.com>
2024-11-07 10:35:36 -0500

    probando

--- database/migrations/2024_11_07_152938_create_categories_table.php
+++ database/migrations/2024_11_07_152938_create_categories_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payments', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-
             $table->timestamps();
-});
+        });
     }
 
     /**
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('payments');
+        Schema::dropIfExists('categories');
     }
 };
